9fe9491b42742b93b47aed4440e35d52edd39deb
Yussuf <yussuf.saumu@gmail.com>
2023-11-29 09:26:28 +0300

    Assignment Complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclaimed_sentences
+result = make_exclamation(["Hello", "My name is Safia"])
+print(result)
